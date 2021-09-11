07a076fb3fc98c4e40dc714760aa0da4baaa9516
Алексей Максименко <ymgreenray@yandex.ru>
2021-09-11 06:07:45 +0300

    Delete test arguments

--- iTransitionTask3/Program.cs
+++ iTransitionTask3/Program.cs
@@ -6,34 +6,26 @@ namespace iTransitionTask3
         {
         static void Main(string[] args)
             {
-            //if (args.Length % 2 == 0)
-            //    {
-            //    FindError("Enter an odd number of arguments");
-            //    }
-            //else if (args.Length < 3)
-            //    {
-            //    FindError("Argements count should be more or equal 3");
-            //    }
-            //string[] sortedArgs = args;
-            //Array.Sort(sortedArgs);
-            //for (int i = 0; i < sortedArgs.Length - 1; i++)
-            //    {
-            //    if (sortedArgs[i] == sortedArgs[i + 1])
-            //        {
-            //        FindError("Arguments must be different");
-            //        }
-            //    }
-
-            string[] arg1 = new string[5];
-            arg1[0] = "rock";
-            arg1[1] = "paper";
-            arg1[2] = "scissors";
-            arg1[3] = "lizard";
-            arg1[4] = "Spock";
-
+            if (args.Length % 2 == 0)
+                {
+                FindError("Enter an odd number of arguments");
+                }
+            else if (args.Length < 3)
+                {
+                FindError("Argements count should be more or equal 3");
+                }
+            string[] sortedArgs = args;
+            Array.Sort(sortedArgs);
+            for (int i = 0; i < sortedArgs.Length - 1; i++)
+                {
+                if (sortedArgs[i] == sortedArgs[i + 1])
+                    {
+                    FindError("Arguments must be different");
+                    }
+                }
 
             Game game = new Game();
-            game.PlayGame(arg1);
+            game.PlayGame(args);
             Environment.Exit(0);
             }
 
